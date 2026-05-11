aa11d245b595faa5f51bcf351fe96ec11c27c854
lintang <lintang.aryasatya.niscala@gmail.com>
2026-05-11 14:16:49 +0700

    first commit

---
+++ push_git.bat
@@ -0,0 +1,5 @@
+@echo off
+"C:\Program Files\Git\cmd\git.exe" remote add origin https://github.com/lintang223/SiPantau.git
+"C:\Program Files\Git\cmd\git.exe" branch -M main
+"C:\Program Files\Git\cmd\git.exe" push -u origin main
+pause
